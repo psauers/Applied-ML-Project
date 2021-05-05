6af2554b5b62b04e98d154ee4778a0c81e6bfa31
Collin Gros <vexprof@gmail.com>
2021-05-04 20:16:16 -0600

    fixed code placement

--- proj/stage3/main.py
+++ proj/stage3/main.py
@@ -72,53 +72,6 @@ from zHelpers import do_feature_scaling
 import os
 
 
-#
-#   Main
-#
-if __name__ == "__main__":
-    # Welcome Message
-    print("Machine Learning Mega Classifier of Bigness Bank Edition!")
-    print("\tThank you for using our software. Copyright 2021 - Team Quokking (Google It!)\n")
-
-    #
-    #   Handle Args
-    #
-    args = get_args()
-
-    #
-    #   Get Dataset From File
-    #
-    filename = args.dataset.replace('.csv', '')     # try to strip filetype, eliminates possible duplicate
-
-    x, y = getDataXY(filename)  # Quits w/ message if unable to open
-
-    # transform non-numerical labels to numerical labels
-    le = LabelEncoder()
-    columns = ["job","marital","education","default","housing","loan","contact","month","poutcome"] #columns to pass to label encoder
-    for c in columns:
-        x[c] = LabelEncoder().fit_transform(x[c])
-
-    y["deposit"] = LabelEncoder().fit_transform(y["deposit"])
-
-    # split newly aquired X and y into seperate datasets for training and testing
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3,
-                                                        random_state=1,
-                                                        stratify=y)
-
-    #
-    #   Data Optimizing
-    #
-
-    # TODO:
-    #   convert string features like job type into enumerated lists
-
-    # Feature Scaling
-    #\/ needs the strings converted first
-    # x_train_std, x_test_std = do_feature_scaling(x_train, x_test)
-
-    #
-    #   Classifier type based actions
-    #
 def do_pcpn(args):
     print("Perceptron selected")
 
@@ -376,50 +329,95 @@ def graph(x, y, x_lab, y_lab, color_):
 
 
 
+#
+#   Main
+#
+if __name__ == "__main__":
+    # Welcome Message
+    print("Machine Learning Mega Classifier of Bigness Bank Edition!")
+    print("\tThank you for using our software. Copyright 2021 - Team Quokking (Google It!)\n")
+
+    #
+    #   Handle Args
+    #
+    args = get_args()
+
+    #
+    #   Get Dataset From File
+    #
+    filename = args.dataset.replace('.csv', '')     # try to strip filetype, eliminates possible duplicate
+
+    x, y = getDataXY(filename)  # Quits w/ message if unable to open
+
+    # transform non-numerical labels to numerical labels
+    le = LabelEncoder()
+    columns = ["job","marital","education","default","housing","loan","contact","month","poutcome"] #columns to pass to label encoder
+    for c in columns:
+        x[c] = LabelEncoder().fit_transform(x[c])
+
+    y["deposit"] = LabelEncoder().fit_transform(y["deposit"])
+
+    # split newly aquired X and y into seperate datasets for training and testing
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3,
+                                                        random_state=1,
+                                                        stratify=y)
+
+    #
+    #   Data Optimizing
+    #
+
+    # TODO:
+    #   convert string features like job type into enumerated lists
+
+    # Feature Scaling
+    #\/ needs the strings converted first
+    # x_train_std, x_test_std = do_feature_scaling(x_train, x_test)
+
+    #
+    #   Classifier type based actions
+    #
+    if args.classifier.lower() == 'pcpn':
+        elapsed, accuracy = do_pcpn(args)
+    elif args.classifier.lower() == 'adaline':
+        elapsed, accuracy = do_adaline(args)
+    elif args.classifier.lower() == 'bagging':
+        elapsed, accuracy = do_bagging(args)
+    elif args.classifier.lower() == 'knn':
+        elapsed, accuracy = do_knn(args)
+    elif args.classifier.lower() == 'svm':
+        elapsed, accuracy = do_svm(args)
+    elif args.classifier.lower() == 'sgd':
+        elapsed, accuracy = do_sgd(args)
+    elif args.classifier.lower() == 'dt':
+        elapsed, accuracy = do_dt(args)
+    elif args.classifier.lower() == 'rf':
+        elapsed, accuracy = do_rf(args)
+    elif args.classifier.lower() == 'adaboost':
+        elapsed, accuracy = do_adaboost(args)
+    # test all models and graph the time and accuracies of them
+    elif args.classifier.lower() == 'test_all':
+        # all models mapped to their function references
+        models = {
+            'pcpn':do_pcpn,
+            'adaline':do_adaline,
+            'bagging':do_bagging,
+            'knn':do_knn,
+            'svm':do_svm,
+            'sgd':do_sgd,
+            'dt':do_dt,
+            'rf':do_rf,
+            'adaboost':do_adaboost
+        }
+        time_l, accuracy_l = test_all(models, args)
+
+        model_l = list(models.keys())
+        graph(model_l, time_l, 'Model', 'Testing_Time', 'green')
+        graph(model_l, accuracy_l, 'Model', 'Accuracy', 'blue')
 
 
-if args.classifier.lower() == 'pcpn':
-    elapsed, accuracy = do_pcpn(args)
-elif args.classifier.lower() == 'adaline':
-    elapsed, accuracy = do_adaline(args)
-elif args.classifier.lower() == 'bagging':
-    elapsed, accuracy = do_bagging(args)
-elif args.classifier.lower() == 'knn':
-    elapsed, accuracy = do_knn(args)
-elif args.classifier.lower() == 'svm':
-    elapsed, accuracy = do_svm(args)
-elif args.classifier.lower() == 'sgd':
-    elapsed, accuracy = do_sgd(args)
-elif args.classifier.lower() == 'dt':
-    elapsed, accuracy = do_dt(args)
-elif args.classifier.lower() == 'rf':
-    elapsed, accuracy = do_rf(args)
-elif args.classifier.lower() == 'adaboost':
-    elapsed, accuracy = do_adaboost(args)
-# test all models and graph the time and accuracies of them
-elif args.classifier.lower() == 'test_all':
-    # all models mapped to their function references
-    models = {
-        'pcpn':do_pcpn,
-        'adaline':do_adaline,
-        'bagging':do_bagging,
-        'knn':do_knn,
-        'svm':do_svm,
-        'sgd':do_sgd,
-        'dt':do_dt,
-        'rf':do_rf,
-        'adaboost':do_adaboost
-    }
-    time_l, accuracy_l = test_all(models, args)
-
-    model_l = list(models.keys())
-    graph(model_l, time_l, 'Model', 'Testing_Time', 'green')
-    graph(model_l, accuracy_l, 'Model', 'Accuracy', 'blue')
-
-
-else:
-    print('Error: Invalid classifier type entered.')
-    print('Valid classifier types:')
-    print('knn, pcpn, adaline, bagging, svm, sgd, dt, rf, and adaboost')
-    quit(-2)
+    else:
+        print('Error: Invalid classifier type entered.')
+        print('Valid classifier types:')
+        print('knn, pcpn, adaline, bagging, svm, sgd, dt, rf, and adaboost')
+        quit(-2)
 
